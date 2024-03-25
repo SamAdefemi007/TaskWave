9534428fcda03f2a3c85d1c3ab585287d8ca8b94
SamAdefemi007 <91951022+SamAdefemi007@users.noreply.github.com>
2024-03-25 11:32:40 -0600

    setup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/homepage/home/home.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HomeComponent, RouterModule],
+  imports: [RouterOutlet, HomeComponent, RouterModule, MatSlideToggleModule],
 })
 export class AppComponent {
   title = 'TaskWave';
