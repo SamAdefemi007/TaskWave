5864862a964857b3ae5c52177b16b0b8061171fd
SamAdefemi007 <91951022+SamAdefemi007@users.noreply.github.com>
2024-03-24 21:33:31 -0600

    added angular material and configured routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/homepage/home/home.component';
+import { DashboardComponent } from './components/dashboardPage/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'home' },
+];
